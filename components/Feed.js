d4e1d34ae60c22764467f39beef3dc9afc89b09a
Ruhaim Mohammed <ruhaimmohammed@gmail.com>
2022-01-25 03:40:28 +0530

    Completed header component

---
+++ components/Feed.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Stories from './Stories';
+
+function Feed() {
+    return (
+        <main>
+            <section>
+                <Stories />
+            </section>
+
+            <section>
+
+            </section>
+        </main>
+    );
+}
+
+export default Feed;
