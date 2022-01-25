ee16e728bd91ce933428301dd37aa4d955cd89af
Ruhaim Mohammed <ruhaimmohammed@gmail.com>
2022-01-26 03:30:45 +0530

    Made customized signin page

--- pages/auth/signin.js
+++ pages/auth/signin.js
@@ -1,15 +1,26 @@
 import { getProviders, signIn as SignIntoProvider } from "next-auth/react";
+import Header from "../../components/Header";
 
 function signIn({ providers }) {
     return (
         <>
-            {Object.values(providers).map((provider) => (
-                <div key={provider.name}>
-                    <button onClick={() => SignIntoProvider(provider.id)}>
-                        Sign in with {provider.name}
-                    </button>
+            <Header />
+
+            <div className="flex flex-col items-center justify-center min-h-screen py-2 -mt-40 px-14 text-center">
+
+                <img className="w-80" src="https://links.papareact.com/ocw" alt="" />
+                <p className="font-xs italic">This is not the REAL INSTAGRAM authorized app, its built for education purpose only</p>
+                <div className="mt-40">
+                    {Object.values(providers).map((provider) => (
+                        <div key={provider.name}>
+                            <button className="p-3 bg-red-400 rounded-lg text-white" onClick={() => SignIntoProvider(provider.id)}>
+                                Sign in with {provider.name}
+                            </button>
+                        </div>
+                    ))}
                 </div>
-            ))}
+            </div>
+
         </>
     );
 }
